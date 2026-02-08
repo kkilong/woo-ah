563cab66a8708a9b91de1d2e402269ca9d68c4ba
kkilong <bapgurht@gmail.com>
2026-02-08 20:06:17 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyAXLrGMg60B_RB06jQFqwAX3_opHnBhUP8",
+    authDomain: "trpg-keyword-2026.firebaseapp.com",
+    projectId: "trpg-keyword-2026",
+    storageBucket: "trpg-keyword-2026.firebasestorage.app",
+    messagingSenderId: "159506261526",
+    appId: "1:159506261526:web:4bbd67d8b7a9fb969e7737",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+</script>
